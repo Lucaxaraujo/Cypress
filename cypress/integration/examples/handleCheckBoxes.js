860b1df5888797b5e8745148dd890941b8ed2521
Lucaxaraujo <lucasm030@gmail.com>
2023-03-01 01:34:12 -0300

    Handle checkboxes

--- cypress/integration/examples/handleCheckBoxes.js
+++ cypress/integration/examples/handleCheckBoxes.js
@@ -5,8 +5,10 @@ describe('My First Test Suite', function() {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
+    cy.get('#checkBoxOption1').uncheck().should('not.be.checked');   
 
-    
+    //Selecting multiple checkboxes 
+    cy.get('input[type="checkbox"]').check(['option2','option3']);
     
   });
 });
